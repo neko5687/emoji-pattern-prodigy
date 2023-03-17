1e45df945c4d571397c33002a0f279cf176dc252
Alina Wiehagen <alina.wiehagen@brightstraining>
2023-03-17 13:47:51 +0100

    add spring

---
+++ spring-epp/src/main/java/com/example/springepp/SpringEppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springepp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEppApplication.class, args);
+	}
+
+}
---
+++ spring-epp/src/test/java/com/example/springepp/SpringEppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springepp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
