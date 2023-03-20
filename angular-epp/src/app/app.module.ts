281c0b18f3d2db27af8c00fb8cdcdcce8fdfb883
Ru79jel <vera-josefine.moultrie@brightstraining.com>
2023-03-20 13:22:30 +0100

    added matrices component

--- angular-epp/src/app/app.module.ts
+++ angular-epp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatricesComponent } from './matrices/matrices.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatricesComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-epp/src/app/matrices/matrices.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-matrices',
+  templateUrl: './matrices.component.html',
+  styleUrls: ['./matrices.component.css']
+})
+export class MatricesComponent {
+
+}
