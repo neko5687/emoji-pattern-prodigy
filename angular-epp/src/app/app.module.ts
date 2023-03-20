708afc1fdad11a73f63ce02a303d45e867e7e3d4
Ru79jel <vera-josefine.moultrie@brightstraining.com>
2023-03-20 14:28:24 +0100

    implemented matrix-component

--- angular-epp/src/app/app.module.ts
+++ angular-epp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatricesComponent } from './matrices/matrices.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MatricesComponent } from './matrices/matrices.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-epp/src/app/matrices/matrices.component.ts
+++ angular-epp/src/app/matrices/matrices.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+interface MatrixDTO {
+  title: String,
+  matrix: String,
+  votes: number,
+  difficulty: number,
+  createdAt: String,
+  creator: String
+}
+
+interface Matrix {
+  title: String,
+  matrix: String[],
+  votes: number,
+  difficulty: String,
+  createdAt: String,
+  creator: String
+}
 
 @Component({
   selector: 'app-matrices',
   templateUrl: './matrices.component.html',
   styleUrls: ['./matrices.component.css']
 })
-export class MatricesComponent {
+export class MatricesComponent implements OnInit{
+  matrices: MatrixDTO[] = [];
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit(): void {
+    this.http.get<MatrixDTO[]>("http://localhost:8080/api/matrices").subscribe(result => this.matrices = result);
+  }
 
+  convertDTOtoMatrix (matrixDto: MatrixDTO) : Matrix {
+    const matrix: Matrix = {
+      title: matrixDto.title,
+      matrix: matrixDto.matrix.match(/\p{Emoji}/gu) || [],
+      votes: matrixDto.votes,
+      difficulty: "🌶️".repeat(matrixDto.difficulty),
+      createdAt: matrixDto.createdAt,
+      creator: matrixDto.creator
+    };
+    return matrix;
+  }
 }
