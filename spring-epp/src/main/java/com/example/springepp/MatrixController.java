306e31ff24b73510854d1172bbcaffcbe712cdc3
Ru79jel <vera-josefine.moultrie@brightstraining.com>
2023-03-20 17:47:11 +0100

    Implemented GetMethod for matrices/id

--- spring-epp/src/main/java/com/example/springepp/MatrixController.java
+++ spring-epp/src/main/java/com/example/springepp/MatrixController.java
@@ -1,12 +1,15 @@
 package com.example.springepp;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin("http://localhost:4200/")
 @RestController
@@ -26,6 +29,20 @@ public class MatrixController {
         for (Matrix matrix : matrixRepository.findAll()) {
             response.add(new MatrixDTO(matrix.getMatrix(), matrix.getTitle(), matrix.getCreatedAt(),
                     matrix.getCreator(), matrix.getVote(), matrix.getDifficulty()));
+        }
+        return response;
+    }
+
+    @GetMapping("/api/matrices/{id}")
+    public MatrixDTO readMatrix(@PathVariable long id) {
+        Optional<Matrix> optionalMatrix = this.matrixRepository.findById(id);
+        MatrixDTO response = new MatrixDTO();
+        if (optionalMatrix.isPresent()) {
+            Matrix matrix = optionalMatrix.get();
+            response = new MatrixDTO(matrix.getMatrix(), matrix.getTitle(), matrix.getCreatedAt(),
+                    matrix.getCreator(), matrix.getVote(), matrix.getDifficulty());
+        } else {
+            //TODO: Errorhandling here
         }
         return response;
     }
