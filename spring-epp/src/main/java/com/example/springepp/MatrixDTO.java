45ceaeeb72c088ea0b805120edd2990665191a64
Alina Wiehagen <alina.wiehagen@brightstraining>
2023-03-21 10:21:20 +0100

    refactor: relating JSon

--- spring-epp/src/main/java/com/example/springepp/MatrixDTO.java
+++ spring-epp/src/main/java/com/example/springepp/MatrixDTO.java
@@ -4,24 +4,26 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.Instant;
-import java.util.List;
+
 @Data
 @NoArgsConstructor
 public class MatrixDTO {
     private String matrix;
+    private long id;
     private String title;
 
     private Instant createdAt;
-    private MatrixUser creator;
+    private String creatorName;
     private int vote;
     private int difficulty;
 
-    public MatrixDTO(String matrix, String title, Instant createdAt, MatrixUser creator, int vote, int difficulty) {
+    public MatrixDTO(String matrix, long id, String title, Instant createdAt, String creatorName, int vote, int difficulty) {
         this.matrix = matrix;
         this.createdAt = createdAt;
-        this.creator = creator;
+        this.creatorName = creatorName;
         this.title = title;
         this.vote = vote;
         this.difficulty = difficulty;
+        this.id=id;
     }
 }
