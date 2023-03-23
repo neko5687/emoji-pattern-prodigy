5adfdab0364b0b541186c0f546f56c04e473aa48
Ru79jel <vera-josefine.moultrie@brightstraining.com>
2023-03-23 12:40:09 +0100

    feat: implement logout in frontend

--- angular-epp/src/app/app.component.ts
+++ angular-epp/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-epp';
+
+  constructor(private router: Router, private http: HttpClient) {
+  }
+
+  logout() {
+    let token = sessionStorage.getItem('token');
+    let payload = {
+      sessionId: "",
+      userName: ""
+    }
+    if (!token) {
+      this.router.navigate(['/login']);
+    } else {
+      payload.sessionId = token;
+      this.http.post<any>('http://localhost:8080/logout', payload).subscribe()
+      sessionStorage.clear();
+    }
+  }
 }
