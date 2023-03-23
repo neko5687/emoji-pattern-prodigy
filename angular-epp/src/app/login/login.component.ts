ad5ad65a7324d5239c230f8a099081d08658033d
Ru79jel <vera-josefine.moultrie@brightstraining.com>
2023-03-23 15:54:31 +0100

    fix: show login button when logged out and vice versa

--- angular-epp/src/app/login/login.component.ts
+++ angular-epp/src/app/login/login.component.ts
@@ -24,7 +24,9 @@ export class LoginComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private http: HttpClient
-  ) { }
+  ) {
+  }
+
 
   ngOnInit() {
     sessionStorage.setItem('token', '');
@@ -32,16 +34,18 @@ export class LoginComponent implements OnInit {
 
   login() {
     this.http.post<any>('http://localhost:8080/login', this.model).subscribe(res => {
-        this.sessionId = res.sessionId;
-        sessionStorage.setItem(
-          'token',
-          this.sessionId
-        );
-        sessionStorage.setItem(
-          'userName',
-          res.userName
-        );
-        this.router.navigate(['/matrices']);
-      }, error => alert("Invalid Username or Password. Login failed."));
+      this.sessionId = res.sessionId;
+      sessionStorage.setItem(
+        'token',
+        this.sessionId
+      );
+      sessionStorage.setItem(
+        'userName',
+        res.userName
+      );
+      this.router.navigate(['/matrices']).then(() => {
+        window.location.reload();
+      });;
+    }, error => alert("Invalid Username or Password. Login failed."));
   }
 }
