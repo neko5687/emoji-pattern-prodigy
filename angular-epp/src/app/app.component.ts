8c1b009665af5b9fc1f49159ee5d443fa8f3ce51
Sebastian Offergeld <sebastian.offergeld@mailbox.org>
2023-03-27 11:00:05 +0200

    style: import darkmode services

--- angular-epp/src/app/app.component.ts
+++ angular-epp/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
+import { Mode } from "./mode-toggle/mode-toggle.model";
+import { ModeToggleService } from "./mode-toggle/mode-toggle.service";
 
 @Component({
   selector: 'app-root',
@@ -11,14 +13,17 @@ export class AppComponent {
   title = 'angular-epp';
   isLoggedIn: boolean;
 
-
-  constructor(private router: Router, private http: HttpClient) {
+  currentMode: Mode = Mode.LIGHT;
+  constructor(private router: Router, private http: HttpClient, private modeToggleService: ModeToggleService) {
     if (sessionStorage.getItem("token") == null) {
       this.isLoggedIn = false;
     }
     else {
       this.isLoggedIn = sessionStorage.getItem("token")?.length != 0;
     }
+    this.modeToggleService.modeChanged$.subscribe((mode: Mode) => {
+      this.currentMode = mode;
+    });
 
   }
 
